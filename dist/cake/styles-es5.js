bfb91a4364b78b0905bc6c9c868243a309da067f
Ashwani <imakumar98@gmail.com>
2019-10-19 23:12:34 +0530

    Done

--- dist/cake/styles-es5.js
+++ dist/cake/styles-es5.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/kajoo/Desktop/CAKE/cake-site/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/ashwani/Public/projects/anwar/cake-site/src/styles.css */"./src/styles.css");
 
 
 /***/ })
